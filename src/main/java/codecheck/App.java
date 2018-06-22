e943c85c4a6df882184749f50693b0bb17a23caf
kondo.kenta <kondo.kenta@tis.co.jp>
2018-06-22 12:26:08 +0900

    a

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -1,11 +1,9 @@
 package codecheck;
 
 import java.io.BufferedReader;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.io.PrintWriter;
 
 public class App {
   public static void main(String[] args) throws Exception {
@@ -20,8 +18,6 @@ public class App {
        conn.setDoOutput(false);
        conn.setDoInput(true);
        conn.connect();
-
-       responseCode = conn.getResponseCode();
 
        StringBuffer sb = new StringBuffer();
        String line = "";
