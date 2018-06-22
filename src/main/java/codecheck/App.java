38666ac26d657476eea8931bfd23ab4b8ef93763
kondo.kenta <kondo.kenta@tis.co.jp>
2018-06-22 11:32:45 +0900

    q

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -7,21 +7,11 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class App {
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args){
 
     for (int i = 0, l = args.length; i < l; i++) {
 
        String output = String.format("argv[%s]: %s", i, args[i]);
-
-       URL url = new URL("http://challenge-server.code-check.io/api/hash");
-       HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-       conn.setRequestMethod("GET");
-       conn.setDoInput(true);
-
-       conn.connect();
-
-       conn.disconnect();
-
        System.out.println(output);
 
         }
